5002c874b3fa7e549ba2f360b5a5938f18e25776
KaLi <lika27@163.com>
2017-12-25 17:05:45 +0800

    test

---
+++ WpfSerialPort/WpfSerialPort/EventHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfSerialPort
+{
+    //class EventHandler
+    //{
+    //}
+
+    public partial class MainWindow : Window
+    {
+
+        #region Event handler for menu items
+        private void TestNoneRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TestRetRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TestNewlineRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TestNewlineRetRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SerialSettingViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AutoSendDataSettingViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SerialCommunicationSettingViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CompactViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HelpMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+
+
+    }
+}
